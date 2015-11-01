2afcde52739a10efa2eee17b8048d0feda48428a
willcai1984 <willcai1984@gmail.com>
2015-11-01 20:02:09 +0800

    Modify picture part func

--- QjdSale/app/src/main/java/com/qjdchina/qjdsale/MemberPicturePlusFragment.java
+++ QjdSale/app/src/main/java/com/qjdchina/qjdsale/MemberPicturePlusFragment.java
@@ -1,77 +1,75 @@
 package com.qjdchina.qjdsale;
-
-
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Gallery;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MemberPicturePlusFragment extends Fragment implements View.OnClickListener {
     private String TAG = "MemberPictureFragment";
+    private View rootView;
+    private Bitmap bitmap;
     private ImageView ivCompanyLicense0;
     private ImageView ivCompanyLicense1;
     private ImageView ivCompanyLicense2;
     private ImageView ivCompanyLicense3;
     private ImageView ivCompanyLicense4;
-    private View rootView;
-    private Bitmap bitmap;
     private int index = 0;
-    private Map<ImageView,Bitmap> mapPic2Bit=new HashMap<ImageView,Bitmap>();
+    private Map<ImageView, Bitmap> mapPic2Bit = new HashMap<ImageView, Bitmap>();
     public static final int REQUEST_CODE_TAKE_PICTURE = 12;
     public static final String SAVE_PATH_IN_SDCARD = "/sdcard/myImage/"; //图片及其他数据保存文件夹
     public static final String IMAGE_CAPTURE_NAME = "cameraTmp.png"; //照片名称
+    //organization_code
+    private ImageView ivOrganizationcode1;
+    //other_certificates
+    private ImageView ivOtherCertificates1;
+    private ImageView ivOtherCertificates2;
+    private ImageView ivOtherCertificates3;
+    private ImageView ivOtherCertificates4;
+    private ImageView ivOtherCertificates5;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_member_picture_plus, container, false);
-        ivCompanyLicense0 = (ImageView) rootView.findViewById(R.id.iv_comany_license0);
-        ivCompanyLicense1 = (ImageView) rootView.findViewById(R.id.iv_comany_license1);
-        ivCompanyLicense2 = (ImageView) rootView.findViewById(R.id.iv_comany_license2);
-        ivCompanyLicense3 = (ImageView) rootView.findViewById(R.id.iv_comany_license3);
-        ivCompanyLicense4 = (ImageView) rootView.findViewById(R.id.iv_comany_license4);
-        //Tag0:not picture yet;Tag1:have picture in this tv
-        ivCompanyLicense0.setTag(0);
-        ivCompanyLicense1.setTag(0);
-        ivCompanyLicense2.setTag(0);
-        ivCompanyLicense3.setTag(0);
-        ivCompanyLicense4.setTag(0);
-        ivCompanyLicense0.setOnClickListener(this);
-        ivCompanyLicense1.setOnClickListener(this);
-        ivCompanyLicense2.setOnClickListener(this);
-        ivCompanyLicense3.setOnClickListener(this);
-        ivCompanyLicense4.setOnClickListener(this);
+        initParameters(rootView);
         return rootView;
     }
 
+    /*
+    初始化各参数
+    */
+    private void initParameters(View view) {
+        ivCompanyLicense0 = (ImageView) initParameter(view, R.id.iv_comany_license0);
+        ivCompanyLicense1 = (ImageView) initParameter(view, R.id.iv_comany_license1);
+        ivCompanyLicense2 = (ImageView) initParameter(view, R.id.iv_comany_license2);
+        ivCompanyLicense3 = (ImageView) initParameter(view, R.id.iv_comany_license3);
+        ivCompanyLicense4 = (ImageView) initParameter(view, R.id.iv_comany_license4);
+        //organizationCode
+        ivOrganizationcode1 = (ImageView) initParameter(view, R.id.iv_organization_code_1);
+        //otherCertificates
+        ivOtherCertificates1 = (ImageView) initParameter(view, R.id.iv_other_certificates_1);
+        ivOtherCertificates2 = (ImageView) initParameter(view, R.id.iv_other_certificates_2);
+        ivOtherCertificates3 = (ImageView) initParameter(view, R.id.iv_other_certificates_3);
+        ivOtherCertificates4 = (ImageView) initParameter(view, R.id.iv_other_certificates_4);
+        ivOtherCertificates5 = (ImageView) initParameter(view, R.id.iv_other_certificates_5);
+    }
+
+    /*
+    处理点击事件
+    */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -123,19 +121,28 @@ public class MemberPicturePlusFragment extends Fragment implements View.OnClickL
         }
     }
 
+    /*
+    启动相机
+    */
+    private void startCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File imgFile = new File(SAVE_PATH_IN_SDCARD, IMAGE_CAPTURE_NAME);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imgFile));
+        startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE);
+    }
+
+    /*
+    相机回送数据
+    */
     @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         processPicture(requestCode, resultCode, data);
     }
 
-    public void startCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File imgFile=new File(SAVE_PATH_IN_SDCARD,IMAGE_CAPTURE_NAME);
-        //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imgFile));
-        startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE);
-    }
-
+    /*
+    图片处理
+    */
     public void processPicture(int requestCode, int resultCode, Intent intent) {
         Log.d(TAG, "Capture picture successfully");
         Bundle b = intent.getExtras();
@@ -144,7 +151,7 @@ public class MemberPicturePlusFragment extends Fragment implements View.OnClickL
             switch (index) {
                 case 0:
                     ivCompanyLicense0.setImageBitmap(bitmap);
-                    mapPic2Bit.put(ivCompanyLicense0,bitmap);
+                    mapPic2Bit.put(ivCompanyLicense0, bitmap);
                     //load captured picture
                     ivCompanyLicense0.setTag(1);
                     ivCompanyLicense1.setVisibility(View.VISIBLE);
@@ -175,4 +182,14 @@ public class MemberPicturePlusFragment extends Fragment implements View.OnClickL
             }
         }
     }
+
+    private View initParameter(View rootView, int id) {
+        View view;
+        view = rootView.findViewById(id);
+        //Tag0:not picture yet;Tag1:have picture in this tv
+        view.setTag(0);
+        view.setOnClickListener(this);
+        return view;
+    }
+
 }
